ea1e3b9e029f0509d6ca3874d88b4743324833f6
ser1c <87962026+ser1c@users.noreply.github.com>
2025-03-04 21:35:47 +0000

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import rehypeKatex from "rehype-katex"
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://ser1c.github.io/",
+    site: "https://sabinsubedi.com/",
     markdown: {
         syntaxHighlight: "shiki",
         shikiConfig: {
